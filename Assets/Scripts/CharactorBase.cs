3bc14f28c09844e44f3417849393658a24dde51a
Es <radwimps.september@gmail.com>
2015-02-09 04:42:47 +0900

    update

--- Assets/Scripts/CharactorBase.cs
+++ Assets/Scripts/CharactorBase.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Es.Charactor
@@ -9,15 +11,67 @@ namespace Es.Charactor
   public abstract class CharactorBase : MonoBehaviour
   {
     /**************************************************
-     * キャラクターステータス
+     * field
      **************************************************/
-    [SerializeField]
+    [SerializeField, Range(0, 999)]
     protected int hp;
     [SerializeField]
     protected float moveSpeed;
 
     /**************************************************
-     * クラスフィールド
+     * method
      **************************************************/
+
+    /// <summary>
+    /// 残りHPを返す
+    /// </summary>
+    /// <returns>残りHP</returns>
+    public int GetHP()
+    {
+      return hp;
+    }
+
+    /// <summary>
+    /// 複数のコライダーのrigidbody2Dに指定した方向に力を加える
+    /// 全てのコライダーは同じ方向に力を与えられる
+    /// </summary>
+    /// <param name="cols">力を与える対象の列挙可能コライダー</param>
+    /// <param name="dir">力の向き</param>
+    /// <param name="power">力の大きさ</param>
+    protected void AddForce(IEnumerable<Collider2D> cols, Vector2 dir, float power)
+    {
+      dir = dir.normalized;
+      foreach(var col in cols)
+      {
+        if(col.rigidbody2D == null)
+        {
+          Debug.LogWarning(col.name + "にrigidbody2Dが適用されていません");
+          continue;
+        }
+        col.rigidbody2D.AddForce(dir * power);
+      }
+    }
+
+    /// <summary>
+    /// 1つのコライダーのrigidbody2Dに指定した方向に力を加える
+    /// </summary>
+    /// <param name="col">力を与える対象のコライダー</param>
+    /// <param name="dir">力の向き</param>
+    /// <param name="power">力の大きさ</param>
+    protected void AddForce(Collider2D col, Vector2 dir, float power)
+    {
+      List<Collider2D> cols = new List<Collider2D>();
+      cols.Add(col);
+      AddForce(cols, dir, power);
+    }
+
+    /// <summary>
+    /// ダメージを受けた時の動作
+    /// </summary>
+    protected virtual void Damaged()
+    {
+      --hp;
+      Debug.Log(name + " 残りHP:" + hp);
+    }
   }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,9 @@ namespace Es.Charactor
   /// </summary>
   internal enum PlayerState
   {
-    Idle,
-    Play,
-
-    //以下、遷移不可時間が存在する
-    Damaged,
-    Dead,
+    Idle,//強制的に動けない状態
+    Play,//ゲームプレイ中の状態
+    Dead,//死亡状態
   }
 
   /// <summary>
@@ -28,76 +25,100 @@ namespace Es.Charactor
   public class PlayerController : CharactorBase
   {
     /**************************************************
-     * Field
+     * field
      **************************************************/
-    private bool possibleChangeState;
-    private PlayerState state;
+
+    [SerializeField]
+    private PlayerState state = PlayerState.Play;
+    [SerializeField]
+    public Transform attackOrigin;
+    [SerializeField, Range(0, 999)]
+    private float attackPower = 999f;
+    [SerializeField, Range(0, 10)]
+    private float attackRadius = 0.5f;
+
     private float horizontalInput;
     private float verticalInput;
-    private bool leftAttackInput;
-    private bool rightAttackInput;
+    private bool leftHandAttackInput;//"左手"での攻撃(右に飛ばす)
+    private bool rightHandAttackInput;//"右手"での攻撃(左に飛ばす)
+    private bool forwardLeftHandAttackInput;//"左手"での攻撃(前に飛ばす)
+    private bool forwardRightHandAttackInput;//"右手"での攻撃(前に飛ばす)
     private Vector2 dir;
-    private Dictionary<string, Animator> handAnimators;
 
     private const string LEFT_HAND = "LeftHand";
     private const string RIGHT_HAND = "RightHand";
 
-    private PlayerController()
-    {
-      possibleChangeState = true;
-      state = PlayerState.Idle;
-    }
-
-    public void Awake()
-    {
-      //ゲームオブジェクト名にhandを含むオブジェクトのAnimatorコンポーネントをHashで取得
-      //キーはゲームオブジェクト名
-      handAnimators = transform.GetComponentsInChildren<Animator>().Where(a =>
-      {
-        return a.name.ToLower().Contains("hand");
-      }).ToDictionary(a => a.name);
-    }
+    /**************************************************
+     * override
+     **************************************************/
 
     public void Update()
     {
-      //ユーザーが状態遷移可能なら
-      if(possibleChangeState)
-      {
-        //ユーザー入力情報の取得
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        leftAttackInput = Input.GetButtonDown("LeftAttack");
-        rightAttackInput = Input.GetButtonDown("RightAttack");
-
-        //PlayerStateの変更
-      }
+      //ユーザー入力情報の取得
+      GetUserInput();
 
       //State別処理
       switch(state)
       {
         case PlayerState.Idle:
+          Debug.Log(name + ":Idle状態に遷移しました");
           break;
 
         case PlayerState.Play:
 
           #region 移動
           dir = new Vector2(horizontalInput, verticalInput);
-          transform.Translate(Time.deltaTime * moveSpeed * dir);
+          transform.Translate(Time.deltaTime * moveSpeed * dir, Space.World);
           #endregion 移動
 
           #region 攻撃
-          if(leftAttackInput)
-            handAnimators[LEFT_HAND].SetTrigger("Attack");
-          if(rightAttackInput)
-            handAnimators[RIGHT_HAND].SetTrigger("Attack");
+
+          //Debug.DrawLine(attackOrigin.position - Vector3.right * attackRadius, attackOrigin.position + Vector3.right * attackRadius);
+          if(leftHandAttackInput || rightHandAttackInput || forwardLeftHandAttackInput || forwardRightHandAttackInput)
+          {
+            /**************************************************
+             * Enemyタグをもつ目の前のコライダーの取得と
+             * 攻撃対象(力を与える対象)の選定
+             **************************************************/
+
+            var cols = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius)
+              .Where(c => { return c.tag == "Enemy"; })//tagがEnemy
+              .Where(c =>
+              {
+                var component = c.GetComponent<CharactorBase>();
+                if(component == null)
+                  return false;
+                return component.GetHP() > 0;
+              })//HPが1以上
+              .Where(c =>
+              {
+                var component = c.GetComponent<EnemyControll>();
+                if(component == null)
+                  return false;
+                return component.State == EnemyState.Play;
+              });//Play状態
+
+            //それぞれの攻撃による吹き飛ばし
+            if(leftHandAttackInput)
+              AddForce(cols, new Vector2(1, -1), attackPower);
+            else if(rightHandAttackInput)
+              AddForce(cols, new Vector2(1, 1), attackPower);
+            else if(forwardLeftHandAttackInput || forwardRightHandAttackInput)
+              AddForce(cols, new Vector2(1, 0), attackPower);
+
+            //対象のステート変更とダメージ処理
+            foreach(var col in cols)
+            {
+              col.GetComponent<EnemyControll>().State = EnemyState.Panched;
+              col.gameObject.SendMessage("Damaged");
+            }
+          }
           #endregion 攻撃
 
           break;
 
-        case PlayerState.Damaged:
-          break;
-
         case PlayerState.Dead:
+          Debug.Log("Dead状態に遷移しました");
           break;
 
         default:
@@ -105,20 +126,21 @@ namespace Es.Charactor
       }
     }
 
-#if DEBUG
+    /**************************************************
+     * method
+     **************************************************/
 
-    public void OnGUI()
+    /// <summary>
+    /// ユーザーからの入力を取得する
+    /// </summary>
+    private void GetUserInput()
     {
-      if(GUILayout.Button("Idle"))
-        state = PlayerState.Idle;
-      if(GUILayout.Button("Play"))
-        state = PlayerState.Play;
-      if(GUILayout.Button("Damaged"))
-        state = PlayerState.Damaged;
-      if(GUILayout.Button("Dead"))
-        state = PlayerState.Dead;
+      horizontalInput = Input.GetAxis("Horizontal");
+      verticalInput = Input.GetAxis("Vertical");
+      leftHandAttackInput = Input.GetButtonDown("LeftHandAttack");
+      rightHandAttackInput = Input.GetButtonDown("RightHandAttack");
+      forwardLeftHandAttackInput = Input.GetButtonDown("ForwardLeftHandAttack");
+      forwardRightHandAttackInput = Input.GetButtonDown("ForwardRightHandAttack");
     }
-
-#endif
   }
 }
